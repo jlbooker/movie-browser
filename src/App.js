1470c726fbcc4b9a7e61ba60d94322d3e9124f7c
Jeremy Booker <jeremy.booker@gmail.com>
2024-07-25 01:40:01 -0400

    Add react-query and get basic API access working.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,29 @@
 import MovieBrowser from "./MovieBrowser";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
     <div className="App">
-      <nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4">
-        <div class="container-fluid">
-          <a class="navbar-brand" href="/">
+      <nav className="navbar navbar-expand-md navbar-dark bg-dark mb-4">
+        <div className="container-fluid">
+          <a className="navbar-brand" href="/">
             Movie Browser
           </a>
         </div>
       </nav>
 
-      <main class="container">
-        <div class="bg-body-tertiary p-5 rounded">
+      <main className="container">
+        <div className="bg-body-tertiary p-5 rounded">
           <h1>Movie Browser</h1>
-          <p class="lead">
+          <p className="lead">
             This quick React demo app for searching and filtering with a movie
             API.
           </p>
-          <MovieBrowser />
+          <QueryClientProvider client={queryClient}>
+            <MovieBrowser />
+          </QueryClientProvider>
         </div>
       </main>
     </div>
--- src/MovieBrowser.js
+++ src/MovieBrowser.js
@@ -1,4 +1,37 @@
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+
+const API_BASE_URI = "https://0kadddxyh3.execute-api.us-east-1.amazonaws.com";
+
 function MovieBrowser() {
+  const queryClient = useQueryClient();
+
+  // API Bearer Token API
+  const { data: apiToken } = useQuery({
+    queryKey: ["token"],
+    queryFn: async () => {
+      const response = await fetch(`${API_BASE_URI}/auth/token`);
+      if (!response.ok) {
+        throw new Error("There was an error while getting an API token.");
+      }
+      return response.json();
+    },
+  });
+
+  // Movies query
+  const moviesQuery = useQuery({
+    queryKey: ["movies"],
+    queryFn: async () => {
+      const response = await fetch(`${API_BASE_URI}/movies`, {
+        headers: { Authorization: `Bearer ${apiToken.token}` },
+      });
+      if (!response.ok) {
+        throw new Error("There was an error while getting an API token.");
+      }
+      return response.json();
+    },
+    enabled: !!apiToken?.token,
+  });
+
   return <div>Movie Browser demo</div>;
 }
 
