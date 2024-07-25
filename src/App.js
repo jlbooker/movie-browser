f9084e325781d312d857a27fa01c43da2d26d30a
Jeremy Booker <jeremy.booker@gmail.com>
2024-07-25 00:19:47 -0400

    More CRA cleanup. Add Bootstrap with basic menu bar and page layout.

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import MovieBrowser from "./MovieBrowser";
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Movie Browser</header>
+      <nav class="navbar navbar-expand-md navbar-dark bg-dark mb-4">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="/">
+            Movie Browser
+          </a>
+        </div>
+      </nav>
+
+      <main class="container">
+        <div class="bg-body-tertiary p-5 rounded">
+          <h1>Movie Browser</h1>
+          <p class="lead">
+            This quick React demo app for searching and filtering with a movie
+            API.
+          </p>
+          <MovieBrowser />
+        </div>
+      </main>
     </div>
   );
 }
---
+++ src/MovieBrowser.js
@@ -0,0 +1,5 @@
+function MovieBrowser() {
+  return <div>Movie Browser demo</div>;
+}
+
+export default MovieBrowser;
