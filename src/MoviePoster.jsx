2ba62f944d467f83bf60a024c3cfe3af19b176ec
Jeremy Booker <jeremy.booker@gmail.com>
2024-07-26 23:41:18 -0400

    Refactor MovieCard to split MoviePoster and MovieRating into their own components

---
+++ src/MoviePoster.jsx
@@ -0,0 +1,34 @@
+function MoviePoster({ movieTitle, posterUrl }) {
+  if (posterUrl === undefined) {
+    // Some titles don't have a poster. Show an SVG placeholder when that happens.
+    return (
+      <svg
+        className="card-img-top"
+        width="100%"
+        height="282"
+        xmlns="http://www.w3.org/2000/svg"
+        role="img"
+        aria-label={`No movie poster image available for ${movieTitle}`}
+        preserveAspectRatio="xMidYMid slice"
+        focusable="false"
+      >
+        <title>Placeholder</title>
+        <rect width="100%" height="100%" fill="#868e96"></rect>
+        <text x="15%" y="50%" fill="#dee2e6" dy=".3em">
+          No poster available
+        </text>
+      </svg>
+    );
+  } else {
+    return (
+      <img
+        src={posterUrl}
+        className="card-img-top"
+        style={{ maxHeight: "282px" }}
+        alt={`Movie poster for ${movieTitle}`}
+      />
+    );
+  }
+}
+
+export default MoviePoster;
